8983ac5dfad7322647739bd0fe71c3d9ff0911cf
gnongsie <gnongsie@visa.com>
2026-08-03 15:42:24 +0530

    Updating service reference for security

--- CyberSource/Client/Service References/SoapServiceReference/Reference.cs
+++ CyberSource/Client/Service References/SoapServiceReference/Reference.cs
@@ -34989,7 +34989,7 @@ namespace CyberSource.Clients.SoapServiceReference {
         
         private string customerUserNameField;
         
-        private string customerPasswordField;
+        private System.Security.SecureString customerPasswordField;
         
         private string ipNetworkAddressField;
         
@@ -35357,10 +35357,11 @@ namespace CyberSource.Clients.SoapServiceReference {
         [System.Xml.Serialization.XmlElementAttribute(Order=23)]
         public string customerPassword {
             get {
-                return this.customerPasswordField;
+                return ConvertToUnsecureString(this.customerPasswordField);
             }
             set {
-                this.customerPasswordField = value;
+                this.customerPasswordField = new System.Net.NetworkCredential(string.Empty, value).SecurePassword;
+                this.passPhraseField.MakeReadOnly();
                 this.RaisePropertyChanged("customerPassword");
             }
         }
@@ -59329,7 +59330,7 @@ namespace CyberSource.Clients.SoapServiceReference {
         
         private string customerHasSavingsAccountField;
         
-        private string customerPasswordChangeField;
+        private System.Security.SecureString customerPasswordChangeField;
         
         private string customerPhoneChangeField;
         
@@ -59545,10 +59546,11 @@ namespace CyberSource.Clients.SoapServiceReference {
         [System.Xml.Serialization.XmlElementAttribute(Order=4)]
         public string customerPasswordChange {
             get {
-                return this.customerPasswordChangeField;
+                return ConvertToUnsecureString(this.customerPasswordChangeField);
             }
             set {
-                this.customerPasswordChangeField = value;
+                this.customerPasswordChangeField = new System.Net.NetworkCredential(string.Empty, value).SecurePassword;
+                this.passPhraseField.MakeReadOnly();
                 this.RaisePropertyChanged("customerPasswordChange");
             }
         }
